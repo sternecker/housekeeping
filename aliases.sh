432b190221890253181af3abdb12130d89e39da3
Geoff Sternecker <sternecker@gmail.com>
2023-11-25 13:04:09 -0500

    Added new files.

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "alias vim=\"nvim\"" | tee -a ~/.bashrc
+source ~/.bashrc
---
+++ gitconfig.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+
+git config --global user.email "sternecker@gmail.com"
+git config --global user.name "Geoff Sternecker"
+git config --global init.defaultBranch main
+git config --global core.editor nvim
