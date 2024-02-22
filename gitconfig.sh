6bc1d46696080205b0cdc7665e8823ff80318247
Geoff Sternecker <sternecker@gmail.com>
2024-02-21 19:51:57 -0500

    added git rebase

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,3 +4,4 @@ git config --global user.email "sternecker@gmail.com"
 git config --global user.name "Geoff Sternecker"
 git config --global init.defaultBranch main
 git config --global core.editor nvim
+git config --global pull.rebase true
