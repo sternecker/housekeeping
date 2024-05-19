9a6e230a4f7e0869a57049947a91c2876778b4b2
Geoff Sternecker <sternecker@gmail.com>
2024-05-19 09:08:50 -0400

    Testing the acp alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -5,3 +5,5 @@ git config --global user.name "Geoff Sternecker"
 git config --global init.defaultBranch main
 git config --global core.editor nvim
 git config --global pull.rebase true
+
+git config --globa alias.acp "!f() { git add . && git commit -m \"$1\" && git push; }; f"
