307b9c870ad8131f2ebc44bec5ecd2b03d7fc026
Geoff Sternecker <sternecker@gmail.com>
2024-11-09 09:58:42 -0500

    Added nvim for fc.

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-echo "alias vim=\"nvim\"" | tee -a ~/.bashrc
-echo "alias code=\"codium\"" | tee -a ~/.bashrc
-source ~/.bashrc
+cd ~/
+if grep -Fxq "alias vim=\"nvim\"" .bashrc
+then
+	echo "skipping"
+else
+	echo "alias vim=\"nvim\"" | tee -a ~/.bashrc
+	echo "alias code=\"codium\"" | tee -a ~/.bashrc
+	echo "export FCEDIT=\"nvim\"" | tee -a ~/.bashrc
+	source ~/.bashrc
+fi
+cd -
